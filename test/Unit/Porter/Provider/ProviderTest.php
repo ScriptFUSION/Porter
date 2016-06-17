09e2d7e9b5b111ee3cf2b76c2eb97e4396c79aa1
Bilge <bilge@scriptfusion.com>
2016-06-17 22:07:35 +0100

    Added Provider tests.

--- test/Unit/Porter/Provider/ProviderTest.php
+++ test/Unit/Porter/Provider/ProviderTest.php
@@ -2,7 +2,9 @@
 namespace ScriptFUSIONTest\Unit\Porter\Provider;
 
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-use Mockery\Mock;
+use Mockery\MockInterface;
+use ScriptFUSION\Porter\Cache\CacheOperationProhibitedException;
+use ScriptFUSION\Porter\Connector\CachingConnector;
 use ScriptFUSION\Porter\Connector\Connector;
 use ScriptFUSION\Porter\Provider\IncompatibleDataTypeException;
 use ScriptFUSION\Porter\Provider\Provider;
@@ -15,14 +17,27 @@ final class ProviderTest extends \PHPUnit_Framework_TestCase
     /** @var Provider */
     private $provider;
 
+    /** @var MockInterface */
     private $connector;
 
     protected function setUp()
     {
+        $this->createProviderMock();
+    }
+
+    private function createProviderMock($connector = null)
+    {
         $this->provider = \Mockery::mock(
             Provider::class,
-            [$this->connector = \Mockery::mock(Connector::class)]
+            [$this->connector = $connector ?: \Mockery::mock(Connector::class)]
         )->makePartial();
+    }
+
+    private function setupCachingConnector()
+    {
+        $this->createProviderMock($connector = \Mockery::mock(CachingConnector::class));
+
+        return $connector;
     }
 
     public function testFetch()
@@ -52,5 +67,51 @@ final class ProviderTest extends \PHPUnit_Framework_TestCase
                 ->andReturn('foo')
                 ->getMock()
         );
+    }
+
+    public function testGetConnector()
+    {
+        self::assertSame($this->connector, $this->provider->getConnector());
+    }
+
+    public function testEnableCacheFails()
+    {
+        $this->setExpectedException(CacheOperationProhibitedException::class);
+
+        $this->provider->enableCache();
+    }
+
+    public function testEnableCacheSucceeds()
+    {
+        $this->setupCachingConnector()->shouldReceive('enableCache');
+
+        $this->provider->enableCache();
+    }
+
+    public function testDisableCacheFails()
+    {
+        $this->setExpectedException(CacheOperationProhibitedException::class);
+
+        $this->provider->disableCache();
+    }
+
+    public function testDisableCacheSucceeds()
+    {
+        $this->setupCachingConnector()->shouldReceive('disableCache');
+
+        $this->provider->disableCache();
+    }
+
+    public function testCacheDisabledWhenConnectorDoesNotSupportCaching()
+    {
+        self::assertFalse($this->provider->isCacheEnabled());
+    }
+
+    public function testCacheEnabledMirrorsCachingConnector()
+    {
+        $this->setupCachingConnector()->shouldReceive('isCacheEnabled')->andReturn(true, false);
+
+        self::assertTrue($this->provider->isCacheEnabled());
+        self::assertFalse($this->provider->isCacheEnabled());
     }
 }
