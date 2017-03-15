390a76d61e91966b2ea2ca6dc16aca1906404df2
Sam Van der Borght <sam.vanderborght@vink.be>
2017-03-15 12:44:34 +0100

    Add unit test for PSR6 reserved characters

---
+++ test/Unit/Porter/Net/Caching/CachingTestConnector.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ScriptFUSIONTest\Unit\Porter\Net\Caching;
+
+use ScriptFUSION\Porter\Connector\CachingConnector;
+use ScriptFUSION\Porter\Options\EncapsulatedOptions;
+
+class CachingTestConnector extends CachingConnector
+{
+
+    public function fetchFreshData($source, EncapsulatedOptions $options = null)
+    {
+        return 'bar';
+    }
+}
