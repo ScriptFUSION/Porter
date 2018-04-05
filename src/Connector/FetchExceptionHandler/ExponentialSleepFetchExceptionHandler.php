07a2445328e6e96ce550296cf20582c5f0df6a34
Bilge <bilge@scriptfusion.com>
2018-04-05 23:30:11 +0100

    Added initial delay constructor parameter to ExponentialSleepFetchExceptionHandler.

--- src/Connector/FetchExceptionHandler/ExponentialSleepFetchExceptionHandler.php
+++ src/Connector/FetchExceptionHandler/ExponentialSleepFetchExceptionHandler.php
@@ -8,11 +8,24 @@ use ScriptFUSION\Retry\ExceptionHandler\ExponentialBackoffExceptionHandler;
  */
 class ExponentialSleepFetchExceptionHandler implements FetchExceptionHandler
 {
+    private $initialDelay;
+
     private $handler;
+
+    /**
+     * Initializes this instance with the specified initial delay. The initial delay will be used when the first
+     * exception is handled; subsequent exceptions will cause longer delays.
+     *
+     * @param int $initialDelay Initial delay.
+     */
+    public function __construct($initialDelay = ExponentialBackoffExceptionHandler::DEFAULT_COEFFICIENT)
+    {
+        $this->initialDelay = $initialDelay | 0;
+    }
 
     public function initialize()
     {
-        $this->handler = new ExponentialBackoffExceptionHandler;
+        $this->handler = new ExponentialBackoffExceptionHandler($this->initialDelay);
     }
 
     public function __invoke(\Exception $exception)
