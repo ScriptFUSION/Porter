9b71bddc34265ec7052c3f5a400d3cf6405cb109
Bilge <bilge@scriptfusion.com>
2019-10-31 09:23:39 +0000

    Added testImportOneOfManyAsync test case.

--- test/Integration/Porter/PorterAsyncTest.php
+++ test/Integration/Porter/PorterAsyncTest.php
@@ -6,6 +6,7 @@ namespace ScriptFUSIONTest\Integration\Porter;
 use Amp\Iterator;
 use Amp\Loop;
 use Amp\Producer;
+use ScriptFUSION\Porter\ImportException;
 use ScriptFUSION\Porter\Porter;
 use ScriptFUSION\Porter\Specification\AsyncImportSpecification;
 use ScriptFUSION\Porter\Transform\FilterTransformer;
@@ -22,6 +23,9 @@ final class PorterAsyncTest extends PorterTest
         $this->specification = new AsyncImportSpecification($this->resource);
     }
 
+    /**
+     * Tests that the full async import path, via connector, resource and provider, fetches a record correctly.
+     */
     public function testImportAsync(): \Generator
     {
         $records = $this->porter->importAsync($this->specification);
@@ -30,9 +34,23 @@ final class PorterAsyncTest extends PorterTest
         self::assertSame(['foo'], $records->getCurrent());
     }
 
+    /**
+     * Tests that the full async import path, via connector, resource and provider, fetches one record correctly.
+     */
     public function testImportOneAsync(): \Generator
     {
         self::assertSame(['foo'], yield $this->porter->importOneAsync($this->specification));
+    }
+
+    /**
+     * Tests that when importOne receives multiple records from a resource, an exception is thrown.
+     */
+    public function testImportOneOfManyAsync(): \Generator
+    {
+        $this->resource->shouldReceive('fetchAsync')->andReturn(Iterator\fromIterable([['foo'], ['bar']]));
+
+        $this->expectException(ImportException::class);
+        yield $this->porter->importOneAsync($this->specification);
     }
 
     /**
